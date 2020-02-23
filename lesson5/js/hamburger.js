5ccc03a82b3154a1d4fc4f2fec5c6dadefd4e5ed
Eakle-Tyler09 <teakle@byui.edu>
2020-02-22 17:46:05 -0700

    css

---
+++ lesson5/js/hamburger.js
@@ -0,0 +1,8 @@
+
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
